c1e0cfb9928c2b9a981ce00236fe30e676fd8531
EH30 <games.eh2000@gmail.com>
2020-06-22 12:16:02 +0530

    Add files via upload

--- src/KeySpam/KeySpam.cpp
+++ src/KeySpam/KeySpam.cpp
@@ -2,35 +2,43 @@
 #include <Windows.h>
 
 
-void keSpam() {
+int get_size_char(char* userch) {
+    int out = 0;
+    for (char* c = userch; *c != '\0'; c++, out++);
+
+    return out;
+}
+
+
+void keSpam(char* userin) {
     INPUT ip;
     int count = 0;
     int count_num = 0;
+    int len = get_size_char(userin);
+    int i = 0;
+    memset(&ip, 0, sizeof(ip));
 
     Sleep(5000);
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.wScan = 0;
-    ip.ki.time = 0;
-    ip.ki.dwExtraInfo = 0;
 
     while (true) {
-        if (count == 5) {
+        if (count >= 5) {
             count = 0;
             Sleep(5000);
         }
 
-        ip.ki.wVk = 0x45;
-        ip.ki.dwFlags = 0;
-        SendInput(1, &ip, sizeof(INPUT));
+        for (i = 0; i < len; i++) {            
+            ip.type = INPUT_KEYBOARD;
+            ip.ki.wVk = VkKeyScanA(userin[i]);
+            SendInput(1, &ip, sizeof(INPUT));
+
+            count_num++;
+            std::cout << "Key Pressed: " << count_num << std::endl;
+        }
+        count++;
 
         ip.ki.wVk = 0x0D;
         ip.ki.dwFlags = 0;
         SendInput(1, &ip, sizeof(INPUT));
-
-        count++;
-        count_num++;
-
-        std::cout << "Key Pressed: " << count_num << std::endl;
     }
 
 }
@@ -38,9 +46,16 @@ void keSpam() {
 
 int main()
 {
+    char userin[100];
+
     std::cout << "      Created By: EH" << std::endl;
     std::cout << "-------------------------------------------" << std::endl;
     
-    keSpam();
+    std::cout << "Enter any letter: ";
+    std::cin.getline(userin, sizeof(userin));
+    keSpam(userin);
+
+    system("pause");
+
     return 0;
 }
